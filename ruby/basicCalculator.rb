47fee7ca5d7879c21395559ae041b39529d81b22
Dr-Aion <aiganym.moldagulova@gmail.com>
2023-11-29 14:56:53 +0400

    ruby Tutorial

---
+++ ruby/basicCalculator.rb
@@ -0,0 +1,5 @@
+puts "Enter the first number: "
+num1 = gets.chomp()
+puts "Enter the second number: "
+num2 = gets.chomp()
+puts(num1.to_i + num2.to_i)
---
+++ ruby/madLibsGame.rb
@@ -0,0 +1,9 @@
+puts "Enter the color: "
+color = gets.chomp()
+puts "Enter the plural: "
+plural = gets.chomp()
+puts "Enter the celebrity: "
+celebrity = gets.chomp()
+puts ("Roses are " + color)
+puts (plural + " are blue")
+puts ("I love " + celebrity)
