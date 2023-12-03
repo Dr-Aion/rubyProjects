7b9919b70a7034ad1f9eb32ed65eed6d4232513a
Dr-Aion <aiganym.moldagulova@gmail.com>
2023-12-03 23:42:11 +0400

    loops

---
+++ ruby/forLoops.rb
@@ -0,0 +1,20 @@
+friends = ["Kevin", "Bob", "Oscar", "Angela", "Andy"]
+# 1
+for friend in friends
+    puts friend
+end
+
+# 2
+friends.each do |friend|
+    puts friend
+end
+
+# 3
+for index in 0..5
+    puts index
+end
+
+#4
+6.times do |index|
+    puts index
+end
